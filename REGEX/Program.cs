f49f7299209f2288ce9c42930f777c826ada5d02
badjateamisha <amishabadjate175@gmail.com>
2022-05-20 12:47:39 +0530

    [Amisha]Entering Valid First Name

---
+++ REGEX/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using REGEX;
+
+Console.WriteLine("Welcome to User Registration Problem Using REGEX!");
+
+UserRegistration userRegistration = new UserRegistration();
+userRegistration.FirstName();
